76e3982305ae9badc5456e8b3fb2d08d2793a285
ahmadf16 <ahmads3amen@gmail.com>
2020-06-08 12:37:32 +0300

    tran system

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const availableLangs = ['en', 'ar'];
 
-let obj = {};
+let obj = { devBot: require(`./lyra/bot.json`), devWeb: require(`./lyra/website.json`) };
+
 for (lang of availableLangs) obj[lang] = {
     bot: require(`./${lang}/bot.json`),
     web: require(`./${lang}/website.json`)
